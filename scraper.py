a07e6afbb3e5532e26ca55dab69b2a4f36a5f63d
Josh Nkoy <josh.nkoy@lob.com>
2022-12-28 12:57:42 -0700

    try uncommenting chromedriver install

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ def parse_website(url: str, dyn: bool) -> str:
         options.add_argument('headless')
         driver = webdriver.Chrome(
             # Try commenting/uncommenting the following if you get webdriver errors
-            # ChromeDriverManager().install(),
+            ChromeDriverManager().install(),
             options=options
         )
         time.sleep(8)
